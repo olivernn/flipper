6e6c009e8c7822e65609c0a7cbc31d7702e47b1d
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2010-06-07 18:12:07 +0100

    initial commit

---
+++ jquery.flipper.js
@@ -0,0 +1,50 @@
+(function($){
+  $.fn.flipper = function(options){
+
+    var defaults = {
+      'perspective': '600',
+      'duration': '2s',
+      'axis': 'Y'
+    }
+
+    var settings = jQuery.extend({}, defaults, options)
+
+    var transformation = 'rotate' + settings.axis + '(180deg)'
+
+    var $container = $(this).parent('#container')
+    var $flipper = $(this)
+    var $front = $(this).find('.front')
+    var $back = $(this).find('.back')
+
+    $container.css({
+      '-webkit-perspective': settings.perspective
+    })
+
+    $flipper.css({
+      '-webkit-transform-style': 'preserve-3d',
+      '-webkit-transition-property': '-webkit-transform',
+      '-webkit-transition-duration': settings.duration
+    }).toggle(function () {
+      $(this).css({
+        '-webkit-transform': transformation
+      })
+    }, function () {
+      $(this).css({
+        '-webkit-transform': ''
+      })
+    })
+
+    $front.css({
+      'position': 'absolute',
+      '-webkit-backface-visibility': 'hidden'
+    })
+
+    $back.css({
+      'position': 'absolute',
+      '-webkit-backface-visibility': 'hidden',
+      '-webkit-transform': transformation
+    })
+
+    return this;
+  };
+})(jQuery);
