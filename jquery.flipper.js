46f9d07972e7959c8d2027fa761618ec0cee771a
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2010-06-08 10:50:00 +0100

    can pass callback to be called when the flipping stops

--- jquery.flipper.js
+++ jquery.flipper.js
@@ -5,53 +5,56 @@
       'perspective': '600',
       'duration': '2s',
       'axis': 'Y',
-      'turns': 0
-    }
+      'turns': 0,
+      'callback': jQuery.noop
+    };
 
-    var settings = jQuery.extend({}, defaults, options)
-    var rotation = (180 + (360 * settings.turns))
-    var transformation = 'rotate' + settings.axis + '(' + rotation + 'deg)'
+    var settings = jQuery.extend({}, defaults, options);
+    var rotation = (180 + (360 * settings.turns));
+    var transformation = 'rotate' + settings.axis + '(' + rotation + 'deg)';
 
-    var $container = jQuery(this).parent('#container')
-    var $flipper = jQuery(this)
-    var $front = jQuery(this).find('.front')
-    var $back = jQuery(this).find('.back')
-    var $target
+    var $container = jQuery(this).parent('#container');
+    var $flipper = jQuery(this);
+    var $front = jQuery(this).find('.front');
+    var $back = jQuery(this).find('.back');
+    var $target;
 
-    settings.target ? $target = jQuery(settings.target) : $target = $flipper
+    settings.target ? $target = jQuery(settings.target) : $target = $flipper;
 
     $container.css({
       '-webkit-perspective': settings.perspective
-    })
+    });
 
     $flipper.css({
       '-webkit-transform-style': 'preserve-3d',
       '-webkit-transition-property': '-webkit-transform',
       '-webkit-transition-duration': settings.duration
-    })
+    });
+
+    document.getElementById($flipper.attr('id')).addEventListener('webkitTransitionEnd', settings.callback, false );
 
     $target.toggle(function () {
       $flipper.css({
         '-webkit-transform': transformation
-      })
+      });
       return false;
     }, function () {
       $flipper.css({
         '-webkit-transform': ''
-      })
+      });
       return false;
-    })
+    });
 
     $front.css({
       'position': 'absolute',
       '-webkit-backface-visibility': 'hidden'
-    })
+    });
 
     $back.css({
       'position': 'absolute',
       '-webkit-backface-visibility': 'hidden',
       '-webkit-transform': transformation
-    })
+    });
 
     return this;
   };
